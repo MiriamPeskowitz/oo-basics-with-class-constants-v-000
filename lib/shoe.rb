1256dea20b5b77be345e27285b4074c0d04b8fb9
Miriam Peskowitz <miriam5000@gmail.com>
2017-09-28 18:32:50 +0000

    automatically backed up by learn

--- lib/shoe.rb
+++ lib/shoe.rb
@@ -2,11 +2,11 @@ class Shoe
   attr_accessor :color, :size, :material, :condition
   attr_reader :brand
   BRANDS = []
-  
+
   def initialize(brand)
     @brand = brand
-    BRANDS << brand unless BRANDS.include?(brand) # question it's a class CONSTANT, doesn't that mean we dont' 
-      # add more elements to this array? 
+    BRANDS << brand unless BRANDS.include?(brand) # question it's a class CONSTANT, doesn't that mean we dont'
+      # add more elements to this array?
   end
   
   def cobble
